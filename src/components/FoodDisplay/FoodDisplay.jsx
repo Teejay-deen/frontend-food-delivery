5cfe1b1777601aafd36274da717368f96347bec2
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-08 00:07:40 +0100

    adding FoodItem

--- src/components/FoodDisplay/FoodDisplay.jsx
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -2,15 +2,23 @@ import React, { useContext } from 'react'
 import "./FoodDisplay.css"
 import { food_list } from '../../assets/assets'
 import { StoreContext } from '../../ContextApi/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
 
     const {food_list} = useContext(StoreContext)
 
 
   return (
-    <div className='food-display'>
-        
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+        <div className="food-display-list">
+            {food_list.map((item, index)=>{
+                return (
+                    <FoodItem key={index} id={item._id} name={item.name} description={item.description} image={item.image} price={item.price} />
+                )
+            })}
+        </div>
     </div>
   )
 }
