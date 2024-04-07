32dd8717bbf4700b79c93a4712c0a0de09d574dc
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-07 13:40:42 +0100

    adding NavBar components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./NavBar.css"
+import { assets } from '../../assets/assets'
 
 const NavBar = () => {
+
+    const [ menu, setMenu] = useState("Home")
+
+
   return (
     <div className='nav-bar '>
+        <img src={assets.logo} alt="" className="logo" />
+        <ul className="navbar-menu">
+            <li onClick={()=>setMenu("Home")} className={menu==="Home" ? "active " : ""}>Home</li>
+            <li onClick={()=>setMenu("Menu")} className={menu==="Menu" ? "active " : ""}>Menu</li>
+            <li onClick={()=>setMenu("Mobile-App")} className={menu==="Mobile-App" ? "active " : ""}>Mobile-App</li>
+            <li onClick={()=>setMenu("Contact-Us")} className={menu==="Contact-Us" ? "active " : ""}>Contact-Us</li>
+        </ul>
 
+        <div className="navbar-right">
+            <img src={assets.search_icon} alt="" />
+            <div className="navbar-search-icon">
+                <img src={assets.basket_icon} alt="" />
+                <div className="dot"></div>
+            </div>
+
+            <button className=''>Sign In</button>
+        </div>
     </div>
   )
 }
