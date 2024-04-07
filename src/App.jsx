ef3baa6f8385922e48cdc449a5907491267a1d28
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-07 13:08:18 +0100

    adding assets file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import NavBar from './components/NavBar/NavBar'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='app'>
+      <NavBar />
+    </div>
   )
 }
 
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./NavBar.css"
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar '>
+
+    </div>
+  )
+}
+
+export default NavBar
