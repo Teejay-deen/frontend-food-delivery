b4be9121d9a3247b6ebc0bf9e86cfac137fbf002
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-04-08 22:09:20 +0100

    adding Cart components

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,34 +1,62 @@
-import React, { useState } from 'react'
-import "./NavBar.css"
-import { assets } from '../../assets/assets'
-import { Link } from 'react-router-dom'
+import React, { useState } from "react";
+import "./NavBar.css";
+import { assets } from "../../assets/assets";
+import { Link } from "react-router-dom";
 
-const NavBar = ({setShowLogin}) => {
-
-    const [ menu, setMenu] = useState("Home")
-
+const NavBar = ({ setShowLogin }) => {
+  const [menu, setMenu] = useState("Home");
 
   return (
-    <div className='nav-bar '>
+    <div className="nav-bar ">
+      <Link to="/">
         <img src={assets.logo} alt="" className="logo" />
-        <ul className="navbar-menu">
-            <Link to="/" onClick={()=>setMenu("Home")} className={menu==="Home" ? "active " : ""}>Home</Link>
-            <a href='#explore-menu' onClick={()=>setMenu("Menu")} className={menu==="Menu" ? "active " : ""}>Menu</a>
-            <a href='#app-download' onClick={()=>setMenu("Mobile-App")} className={menu==="Mobile-App" ? "active " : ""}>Mobile-App</a>
-            <a href='#footer' onClick={()=>setMenu("Contact-Us")} className={menu==="Contact-Us" ? "active " : ""}>Contact-Us</a>
-        </ul>
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("Home")}
+          className={menu === "Home" ? "active " : ""}
+        >
+          Home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("Menu")}
+          className={menu === "Menu" ? "active " : ""}
+        >
+          Menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("Mobile-App")}
+          className={menu === "Mobile-App" ? "active " : ""}
+        >
+          Mobile-App
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("Contact-Us")}
+          className={menu === "Contact-Us" ? "active " : ""}
+        >
+          Contact-Us
+        </a>
+      </ul>
 
-        <div className="navbar-right">
-            <img src={assets.search_icon} alt="" />
-            <div className="navbar-search-icon">
-                <img src={assets.basket_icon} alt="" />
-                <div className="dot"></div>
-            </div>
-
-            <button onClick={()=>setShowLogin(true)} className=''>Sign In</button>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className="dot"></div>
         </div>
-    </div>
-  )
-}
 
-export default NavBar
+        <button onClick={() => setShowLogin(true)} className="">
+          Sign In
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,10 +1,41 @@
-import React from 'react'
-import "./Cart.css"
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../ContextApi/StoreContext";
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
-}
+  const { cartItem, food_list, removeFromCart } = useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Titles</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+
+        <br />
+        <hr />  
+        {food_list.map((item, index)=>{
+          if (cartItem[item._id]> 0 ){
+            return (
+             <div key={index} className="cart-items-title cart-items-item">
+              <img src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>${item.price}</p>
+              <p>{cartItem[item._id]}</p>
+              <p>{item.price*cartItem[item._id]}</p>
+              <p>x</p>
+             </div>
+            )
+          }
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
