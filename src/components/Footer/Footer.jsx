5146c060d5cb38ed8e60b86c16dc2d6f1d2df5f7
Jimoh Tajudeen <jimoh.tajudeen1017@gmail.com>
2024-08-27 12:13:33 +0100

    refactor

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
           </p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
+            <img  src={assets.twitter_icon} alt="" />
             <img src={assets.linkedin_icon} alt="" />
           </div>
         </div>
